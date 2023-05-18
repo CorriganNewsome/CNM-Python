4c1bbe7b2acca5c140ff957e1e46f321e50e5e31
Corrigan Newsome <corrigannewsome@gmail.com>
2023-05-18 13:48:51 -0600

    took inputs and used arithmetic

---
+++ Hello.py
@@ -0,0 +1,14 @@
+# Input Demo
+
+# Get first number from user
+num1_str1 = input('Please enter a number: ')
+# Convert to number
+num1 = float(num1_str1)
+# Get second number from user
+num2_str2 = input('Please enter another number: ')
+# Convert to number
+num2 = float(num2_str2)
+# Add numbers together
+sum = num1 + num2
+#Display result
+print('The sum of your numbers is', sum)
