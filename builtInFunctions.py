be64579e0752f6c7a5627a65b7e0150579392fc1
Corrigan Newsome <corrigannewsome@gmail.com>
2023-05-18 14:17:27 -0600

    built in functions examples

---
+++ builtInFunctions.py
@@ -0,0 +1,17 @@
+# Built In Functions
+
+# Get a number from user and display
+num = float(input('Please enter a number: '))
+absolute = abs(num)
+print('The absolute value is', absolute)
+
+# Find the miminum of two numbers
+num1 = float(input('Please enter a number: '))
+num2 = float(input('Please enter a number: '))
+minimumValue = min(num1, num2)
+print('The lowest of the two numbers is', minimumValue)
+
+# Round a number
+num = float(input("Please enter a decimal number: "))
+roundedValue = round(num)
+print('The value round is', roundedValue)
