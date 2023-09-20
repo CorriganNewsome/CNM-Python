32db3d45dc4eb6bb57f16c430af6aaeaf0352883
Corrigan Newsome <corrigannewsome@Corrigans-MBP.home>
2023-09-19 21:25:34 -0600

    fixed surface area

--- NewsomeP1.py
+++ NewsomeP1.py
@@ -1,21 +1,19 @@
 # Programmer: Corrigan Newsome
 # Email: cnewsome2@cnm.edu
-# Purpose: To help user calculate the surface area and volume of a square
-# pyramid
+# Purpose: To help user calculate the surface area and volume of a square pyramid.
 
 # Import Math Library
 import math
 
 # Recieve inputs from user
 baseLength = float(
-    input('Please enter the are of the base(in feet): '))
+    input('Please enter the length of the base(in feet): '))
 height = float(
-    input('Please enter slant height of pyramid(in feet): '))
+    input('Please enter height of pyramid(in feet): '))
 
 # Create formulas
-slantHeight = math.sqrt((height**2) + (baseLength/2)**2)
 
-surfaceArea = 1/2 * (math.sqrt(baseLength**2) + 4 * (height))
+surfaceArea = baseLength * (math.sqrt(baseLength**2 + 4 * (height**2)))
 
 volume = ((baseLength**2) * height)/3
 
