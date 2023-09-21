4bfb99c9b82e58d068f9bb5df86e5f030334d612
Corrigan Newsome <corrigannewsome@Corrigans-MBP.home>
2023-09-20 19:47:32 -0600

    added p2 instructions

---
+++ Assignments/NewsomeP2.py
@@ -0,0 +1,23 @@
+# NewsomeP2
+# Programmer: Corrigan Newsome
+# Email: cnewsome2@cnm.edu
+# Purpose: Provides user capability to view contact info
+
+
+# 1.	Your program will initialize lists to store information about the states.
+# You should have one list for the names of the states,
+# one list for the names of the capitals,
+# one list for the number of districts per state and one list
+# for the order number the state joined the union.
+# Alternatively you can use a db list of lists as demonstrated in the
+# exercises in the book to store the information in one structure.
+
+# 2.	Next ask the user to enter a name of a state.
+
+# 3.	Find the index of that name.
+
+# 4.	Use that index to report back to the user the name of the capital,
+# the number of congressional districts and the order the state joined the
+# union.
+
+# 5.	Program should look something like this:
