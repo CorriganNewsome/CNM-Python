6f9d9886171f616518be48046d6413721f35ad22
Corrigan Newsome <corrigannewsome@Corrigans-MacBook-Pro.local>
2023-09-27 10:08:37 -0600

    completed P2

--- Assignments/NewsomeP1.py
+++ Assignments/NewsomeP1.py
@@ -1,16 +1,14 @@
 # Programmer: Corrigan Newsome
 # Email: cnewsome2@cnm.edu
-# Purpose: To help user calculate the surface area and volume of a square 
+# Purpose: To help user calculate the surface area and volume of a square
 # pyramid.
 
 # Import Math Library
 import math
 
 # Recieve inputs from user
-baseLength = float(
-    input('Please enter the length of the base(in feet): '))
-height = float(
-    input('Please enter height of pyramid(in feet): '))
+baseLength = float(input("Please enter the length of the base(in feet): "))
+height = float(input("Please enter height of pyramid(in feet): "))
 
 # Create formulas
 
@@ -20,8 +18,8 @@ surfaceArea = baseLength * (math.sqrt(baseLength**2 + 4 * (height**2)))
 
 # pyramidSide = slantHeight * (baseLength / 2)
 
-volume = ((baseLength**2) * height)/3
+volume = ((baseLength**2) * height) / 3
 
 # Print results
-print('The surface area is', surfaceArea)
-print('The volume is', volume)
+print("The surface area is", surfaceArea)
+print("The volume is", volume)
--- Sequencing/indexing.py
+++ Sequencing/indexing.py
@@ -8,5 +8,5 @@ print(mylist[0])
 
 
 # Tupals. Like a list but can't be changed.
-myTuple = (1, 'a', 'blah', 1234)
+myTuple = (1, "a", "blah", 1234)
 print(myTuple[-1])
