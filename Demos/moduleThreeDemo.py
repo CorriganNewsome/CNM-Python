88ee6fa11fa5411e84b6f5d2503fe03c72d98561
Corrigan Newsome <corrigannewsome@Corrigans-MBP.home>
2023-09-28 19:10:46 -0600

    added more demos

---
+++ Demos/moduleThreeDemo.py
@@ -0,0 +1,36 @@
+website = "http://www.python.org"
+new_website = website[:-3] + "com"
+print(new_website)
+
+
+## Stiring Formatting Demo
+
+# Ask for user name
+name = input("Please enter your name: ")
+# Ask for height of wall
+height = float(input("Please enter height of wall in ft: "))
+# Ask for width of wall
+width = float(input("Please enter width of wall in ft: "))
+# Ask for sq ft per gallon
+sqft = float(input("Please enter sqft of gallon coverage of paint: "))
+# Ask for number of coats of paint
+numCoats = int(input("Please enter number of coats of paint: "))
+
+# Calculate how many gallons are needed
+area = height * width  # sqft
+gallonsForOneCoat = area / sqft  # gallons
+gallons = gallonsForOneCoat * numCoats  # gallons
+
+# Display results
+print(
+    name,
+    "the number of gallons needed for a wall that is",
+    height,
+    "ft high and",
+    width,
+    "ft wide with",
+    numCoats,
+    "coats is",
+    gallons,
+    "gallons",
+)
