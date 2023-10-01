69ee8e3a44e21d362cad4ac4fb175236784b876c
Corrigan Newsome <corrigannewsome@Corrigans-MBP.home>
2023-10-01 14:27:13 -0600

    newsomep4

---
+++ Assignments/NewsomeP4.py
@@ -0,0 +1,13 @@
+# NewsomeP3
+# Programmer: Corrigan Newsome
+# Email: cnewsome2@cnm.edu
+# Purpose: Utlizing the dictionary libray in python
+
+
+# Creates a dictionary with common phrases in language a as the key and the translation in another language 2 as the values.
+
+# Display a list of the phrases of language 1 to the user. Hint: use the .keys() method of the dictionary class.
+
+# Ask the user to type in a phrase to translate.
+
+# Display the translation of that phrase to the user.
