ed91f74020ed4a81db6914918d0ecc0371baf68f
Corrigan Newsome <corrigannewsome@Corrigans-MacBook-Pro.local>
2023-10-06 19:53:03 -0600

    finished

--- Assignments/NewsomeP4.py
+++ Assignments/NewsomeP4.py
@@ -6,13 +6,13 @@
 
 # Creates a dictionary with common phrases in language a as the key and the translation in another language 2 as the values.
 frenchPhrases = {
-    "Comment allez vous": "How are you?",
+    "Comment allez vous?": "How are you?",
     "Bonjour": "Hello",
     "Merci": "Thank You",
     "Je ne comprends pas": "I don't understand",
 }
 # Display a list of the phrases of language 1 to the user. Hint: use the .keys() method of the dictionary class.
-print(" Select on of these phrases:", "    ".join(frenchPhrases.keys()))
+print(" Select on of these phrases:".join(frenchPhrases.keys()))
 
 # Ask the user to type in a phrase to translate.
 sentence = input("Please enter a french phrase: ")
