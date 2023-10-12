538d7663e66b02097bd1f9aaa49d10cbc244a7c4
Corrigan Newsome <corrigannewsome@Corrigans-MacBook-Pro.local>
2023-10-12 12:54:18 -0600

    checking ssh

--- Assignments/NewsomeP6.py
+++ Assignments/NewsomeP6.py
@@ -8,3 +8,15 @@
 ## A “header” function that takes no parameters and returns nothing that displays a header. The header will print a summary explaining the purpose of the program.
 ## A “get_location” function that takes no parameters, asks the user for a latitude and longitude and returns a tuple or list with the latitude and longitude. Make sure you tell the user what units to enter their information in!
 ## A “distance” function that takes two tuples, each with a latitude and longitude, calculates the distance between those two geographic points and returns the distance.
+
+# In the main part of the program (after declaring your functions):
+# Call the Header function that displays a header.
+# Program should allow the user to do multiple calculations. Use a “do another?” loop.
+# Inside the loop the program will do the following:
+# Call the get_location function to get the first location.
+# Call the get_location function again to get the second location.
+# Call the distance function passing in the two locations above as arguments.
+# Display a nicely formatted message to the user telling them the distance between those two locations.
+# Finally ask the user if they want to do another.
+# When done display a good bye message outside the loop.
+# use to push
