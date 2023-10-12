dfb68fb9b97837e04397aa9e4efab6c67a2b1906
Corrigan Newsome <corrigannewsome@Corrigans-MacBook-Pro.local>
2023-10-12 12:44:04 -0600

    added P6

---
+++ Assignments/NewsomeP6.py
@@ -0,0 +1,10 @@
+# NewsomeP5
+# Programmer: Corrigan Newsome
+# Email: cnewsome2@cnm.edu
+# Purpose: Use functions to calcuolate disance between two points.
+
+# You are going to write a program that calculates the distance between two geographic points.
+# Your program will have the following functions
+## A “header” function that takes no parameters and returns nothing that displays a header. The header will print a summary explaining the purpose of the program.
+## A “get_location” function that takes no parameters, asks the user for a latitude and longitude and returns a tuple or list with the latitude and longitude. Make sure you tell the user what units to enter their information in!
+## A “distance” function that takes two tuples, each with a latitude and longitude, calculates the distance between those two geographic points and returns the distance.
