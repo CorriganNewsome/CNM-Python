b787b9c347621e44800aef28f1128c476a588909
Corrigan Newsome <corrigannewsome@gmail.com>
2023-10-26 16:23:13 -0600

    added pizza constructor demo

---
+++ Demos/pizza.py
@@ -0,0 +1,10 @@
+class Pizza:
+    def __init__(self, crust, size, toppings):
+        self.crust = "hand tossed"
+        self.size = "Large"
+        self.toppings = ["Cheese"]
+
+
+first_pizza = Pizza()
+
+first_pizza.crust = "Thin"
